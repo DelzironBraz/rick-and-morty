2798bbe606d7fcc06cc3614b5e78fdaaabdaacbe
Delziron Braz <delziron.braz@hotmail.com>
2023-11-04 12:58:32 -0300

    feat: create filter cards by name

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,15 +2,16 @@ import { useEffect, useState } from 'react';
 import './Home.scss';
 import { getCharacters } from '../../utils/fetchFromApi';
 import CardsGrid from '../../components/Grid/CardsGrid';
-import { Box, Pagination } from '@mui/material';
+import { Box, Pagination, TextField } from '@mui/material';
 
 const Home = () => {
     const [characters, setCharacters] = useState([]);
     const [page, setPage] = useState(1);
     const [count, setCount] = useState(0);
+    const [searchName, setSearchName] = useState('');
 
     useEffect(() => {
-        getCharacters(page)
+        getCharacters(page, searchName)
             .then(({ info: { pages }, results }) => {
                 setCharacters(results);
                 setCount(pages);
@@ -18,14 +19,30 @@ const Home = () => {
             .catch((error) => {
                 console.error('Error:', error);
             })
-    });
+    }, [page, searchName]);
 
     const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
         setPage(value);
     };
 
+    const handleSearch = (event) => {
+        event.preventDefault();
+        setSearchName(event.target.value);
+        setPage(1);
+    }
+
     return (
         <Box sx={{ width: '100%', height: '100vh' }} >
+            <Box sx={{ width: '80%', padding: '1rem', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                <TextField
+                    sx={{ width: '40%', }}
+                    value={searchName}
+                    id="outlined-basic"
+                    label="Outlined"
+                    variant="outlined"
+                    onChange={handleSearch}
+                />
+            </Box>
             <CardsGrid results={characters} />
             <Pagination
                 sx={{ width: '100%', display: 'flex', justifyContent: 'center', paddingY: '2rem' }}
