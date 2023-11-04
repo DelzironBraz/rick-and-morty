7505b106f43f4dada607c0c6bc3c707df1db834f
Delziron Braz <delziron.braz@hotmail.com>
2023-11-03 23:21:53 -0300

    feat: filter characters by name

--- src/utils/fecthFromApi.ts
+++ src/utils/fecthFromApi.ts
@@ -17,3 +17,17 @@ export const getCharacters = async () => {
         console.error('Enable to get characters. ', error);
     }
 }
+
+export const filterCharactersByName = async (name: string) => {
+    try {
+        const response = await axios.get(`${BASE_URL}${characters}/?name=${name}`, { responseType: 'json' });
+
+        if (response.status != 200) {
+            throw new Error("Error when fecthing.");
+        }
+
+        return response.data;
+    } catch (error) {
+        console.error('Enable to get characters. ', error);
+    }
+}
